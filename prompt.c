a5a7ccc5de8cb40f3c38d8a21cdfe2325af4a5e4
Tuyishimire Ange Victorien <68068538+Tuyishimire-lab@users.noreply.github.com>
2021-08-15 11:52:39 +0200

    Update prompt.c

--- prompt.c
+++ prompt.c
@@ -2,8 +2,16 @@
 #include "shell.h"
 
 void print_prompt1(void)
-{
-    fprintf(stderr, "$ ");
+{   
+    struct symtab_entry_s *entry = get_symtab_entry("PS1");
+    if(entry && entry->val)
+    {
+        fprintf(stderr, "%s", entry->val);
+    }
+    else
+    {
+        fprintf(stderr, "$ ");
+    }
 }
 void print_prompt2(void)
 {
